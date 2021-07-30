9964f315bd61846cd8619f9045a4cc29fd40b5a3
박혜빈 <2711-happ-in@users.noreply.lab.ssafy.com>
2021-07-30 11:27:59 +0900

    Add: Init

---
+++ backend/bear/src/main/java/com/zootopia/bear/BearApplication.java
@@ -0,0 +1,13 @@
+package com.zootopia.bear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BearApplication.class, args);
+	}
+
+}
---
+++ backend/bear/src/test/java/com/zootopia/bear/BearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zootopia.bear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
