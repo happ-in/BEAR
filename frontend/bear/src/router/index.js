4e97906401ab4f4857cf711aa08fa50a8a6e1aa6
yeji <yeahaluu@gmail.com>
2021-08-03 01:58:05 +0900

    Feat: Add RecomendationPage html
    Related to: #S05P13A403-27

--- frontend/bear/src/router/index.js
+++ frontend/bear/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import Recomendation from "../views/RecomendationPage.vue";
 
 const routes = [
   {
@@ -15,6 +16,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/recomendation",
+    name: "Recomendation",
+    component: Recomendation,
   },
 ];
 
