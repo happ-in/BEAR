ed868840d9db037edebad6e21e322cf6bfff6eaf
박혜빈 <2711-happ-in@users.noreply.lab.ssafy.com>
2021-08-05 15:31:11 +0900

    Chore: Remove relationship - BeerHashTag Entity

--- backend/bear/src/main/java/com/zootopia/bear/Review/service/ReviewRegistryService.java
+++ backend/bear/src/main/java/com/zootopia/bear/Review/service/ReviewRegistryService.java
@@ -3,14 +3,9 @@ package com.zootopia.bear.Review.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.zootopia.bear.Beer.domain.Beer;
-import com.zootopia.bear.Beer.repository.BeerRepository;
-import com.zootopia.bear.HashTag.domain.BeerHashTag;
-import com.zootopia.bear.HashTag.domain.BeerHashTagId;
 import com.zootopia.bear.HashTag.domain.HashTag;
 import com.zootopia.bear.HashTag.domain.ReviewHashTag;
 import com.zootopia.bear.HashTag.dto.HashTagDto;
-import com.zootopia.bear.HashTag.repository.BeerHashTagRepository;
 import com.zootopia.bear.HashTag.repository.HashTagRepository;
 import com.zootopia.bear.HashTag.repository.ReviewHashTagRepository;
 import com.zootopia.bear.Review.domain.Review;
@@ -23,8 +18,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ReviewRegistryService {
 
-	private final BeerRepository beerRepository;
-	private final BeerHashTagRepository beerHashTagRepository;
 	private final HashTagRepository hashTagRepository;
 	private final ReviewRepository reviewRepository;
 	private final ReviewHashTagRepository reviewHashTagRepository;
@@ -33,7 +26,6 @@ public class ReviewRegistryService {
 	public void registryReview(ReviewRegistryDto reviewRegistryDto) {
 		// 1. 리뷰 등록
 		Review review = reviewRepository.save(Review.createReview(reviewRegistryDto));
-		Beer beer = beerRepository.findById(review.getBeerId()).orElseThrow(NullPointerException::new);
 
 		for (HashTagDto hashTagDto : reviewRegistryDto.getHashTags()) {
 			// 1. 해시태그 등록
@@ -43,12 +35,6 @@ public class ReviewRegistryService {
 
 			// 2. 리뷰-해시태그 등록
 			reviewHashTagRepository.save(new ReviewHashTag(review, hashTag));
-
-			// 3. 맥주-해시태그 등록
-			BeerHashTag beerHashTag =
-				beerHashTagRepository.findById(new BeerHashTagId(beer.getBeerId(), hashTag.getHashTagId()))
-					.orElseGet(() -> beerHashTagRepository.save(new BeerHashTag(beer, hashTag)));
-			beerHashTag.addCount();
 		}
 	}
 }
