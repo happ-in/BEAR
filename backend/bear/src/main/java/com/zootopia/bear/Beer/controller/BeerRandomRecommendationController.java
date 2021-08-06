8ef402b3c08784e1adff7c8d90533e7bf17ad9fc
박혜빈 <2711-happ-in@users.noreply.lab.ssafy.com>
2021-08-06 14:53:36 +0900

    Feat: Feature Beer Random Recommendation API

---
+++ backend/bear/src/main/java/com/zootopia/bear/Beer/controller/BeerRandomRecommendationController.java
@@ -0,0 +1,26 @@
+package com.zootopia.bear.Beer.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zootopia.bear.Beer.dto.BeerRandomRecommendationDto;
+import com.zootopia.bear.Beer.service.BeerRandomRecommendationService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping(value = "/beer/random")
+@RequiredArgsConstructor
+public class BeerRandomRecommendationController {
+
+	private final BeerRandomRecommendationService beerRandomRecommendationService;
+
+	@GetMapping
+	public ResponseEntity<?> beerRandomRecommendation() {
+		BeerRandomRecommendationDto beerRandomRecommendationDto = beerRandomRecommendationService.beerRandomRecommendation();
+		return new ResponseEntity<>(beerRandomRecommendationDto, HttpStatus.OK);
+	}
+}
