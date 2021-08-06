c6995353b39b96b56694f31e197c575945cc8bd0
박혜빈 <2711-happ-in@users.noreply.lab.ssafy.com>
2021-08-06 14:57:01 +0900

    Feat: Feature Beer Random Recommendation API

--- backend/bear/src/main/java/com/zootopia/bear/Beer/repository/BeerRepository.java
+++ backend/bear/src/main/java/com/zootopia/bear/Beer/repository/BeerRepository.java
@@ -3,10 +3,14 @@ package com.zootopia.bear.Beer.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.zootopia.bear.Beer.domain.Beer;
 
 public interface BeerRepository extends JpaRepository<Beer, Integer> {
 	@Override
-	Optional<Beer> findById(Integer id);
+	Optional<Beer> findById(Integer beerId);
+
+	@Query(value = "select * from beer order by rand() limit 1", nativeQuery = true)
+	Beer searchRandomBeer();
 }
