b689693a19382c260562b4439b104fcd169da5ef
yeji <yeahaluu@gmail.com>
2021-08-09 04:56:58 +0900

    Remove: Change 초기 컴포넌트 Home, About 삭제

--- frontend/bear/src/App.vue
+++ frontend/bear/src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
     <router-link to="/recommendation">Recommendation</router-link>
   </div>
   <router-view />
@@ -14,6 +12,7 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
 }
 
 #nav {
