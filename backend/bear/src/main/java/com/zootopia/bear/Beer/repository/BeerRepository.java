cec4e157761fc3b161c2437d43a202337a52c64b
happ-in <gpqls304@naver.com>
2021-08-09 14:59:27 +0900

    Fix: Fix merge conflict
    Signed-off-by: happ-in <gpqls304@naver.com>

--- backend/bear/src/main/java/com/zootopia/bear/Beer/repository/BeerRepository.java
+++ backend/bear/src/main/java/com/zootopia/bear/Beer/repository/BeerRepository.java
@@ -7,14 +7,8 @@ import org.springframework.data.jpa.repository.Query;
 
 import com.zootopia.bear.Beer.domain.Beer;
 
-<<<<<<< HEAD
-public interface BeerRepository extends JpaRepository<Beer, Integer>, BeerRepositoryCustom {
-	@Override
-	Optional<Beer> findById(Integer beerId);
-=======
 public interface BeerRepository extends JpaRepository<Beer, Integer> {
 	Beer findById(int id);
->>>>>>> d5842e9edf429c209615a7c451f3d90c9dfc674b
 
 	@Query(value = "select * from beer order by rand() limit 1", nativeQuery = true)
 	Beer searchRandomBeer();
