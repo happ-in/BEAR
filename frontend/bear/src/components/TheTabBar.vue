6f7de6ecb0aefdd3909aa7717a2eba133514f211
yeji <yeahaluu@gmail.com>
2021-08-09 17:06:08 +0900

    Design: Change recommendationpage

--- frontend/bear/src/components/TheTabBar.vue
+++ frontend/bear/src/components/TheTabBar.vue
@@ -35,44 +35,6 @@
             
         }, //컴포넌트가 생성되면 실행
         mounted() {
-            // // Hide Header on on scroll down 
-            // var didScroll; 
-            // var lastScrollTop = 0; 
-            // var delta = 5; 
-            // var navbarHeight = $('header').outerHeight(); 
-            
-            // $(window).scroll(function(event){ 
-            //     didScroll = true; 
-            //     }); 
-                
-            // setInterval(function() { 
-            //     if (didScroll) { 
-            //         hasScrolled(); 
-            //         didScroll = false; 
-            //     } 
-            // }, 250); 
-
-            // function hasScrolled() { 
-            //     var st = $(this).scrollTop(); 
-                
-            //     // Make sure they scroll more than delta 
-            //     if(Math.abs(lastScrollTop - st) <= delta) 
-            //         return; 
-                    
-            //     // If they scrolled down and are past the navbar, add class .nav-up. 
-            //     // This is necessary so you never see what is "behind" the navbar. 
-            //     if (st > lastScrollTop && st > navbarHeight){ 
-            //         // Scroll Down 
-            //         $('header').removeClass('nav-down').addClass('nav-up'); 
-            //     } else { 
-            //         // Scroll Up 
-            //         if(st + $(window).height() < $(document).height()) {
-            //             $('header').removeClass('nav-up').addClass('nav-down'); 
-            //         } 
-            //     } 
-                
-            //     lastScrollTop = st; 
-            // }
         }, //template에 정의된 html코드가 레너링된 후 실행
         unmounted() {}, //unmount가 완료된 후 실행
         methods: {
