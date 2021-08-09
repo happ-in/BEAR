87b85e6c20df64b3cf22d2c9d5b534460c8c8d20
happ-in <gpqls304@naver.com>
2021-08-09 14:40:47 +0900

    Refactor: Refactor beer domain
    - Join Country Table (fetch EAGER)
    
    Signed-off-by: happ-in <gpqls304@naver.com>

--- backend/bear/src/main/java/com/zootopia/bear/Beer/dto/BeerHashTagDto.java
+++ backend/bear/src/main/java/com/zootopia/bear/Beer/dto/BeerHashTagDto.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.zootopia.bear.Beer.domain.Beer;
+import com.zootopia.bear.Country.domain.Country;
 import com.zootopia.bear.HashTag.dto.HashTagTotalDto;
 
 import lombok.AllArgsConstructor;
@@ -16,8 +17,8 @@ import lombok.Setter;
 public class BeerHashTagDto {
 	private int beerId;
 	private String beerName;
-	private String countryName;
 	private String beerCategory;
+	private Country country;
 	private double alcoholProof;
 	private String beerImage;
 	private List<HashTagTotalDto> hashTags = new ArrayList<>();
@@ -25,7 +26,7 @@ public class BeerHashTagDto {
 	public BeerHashTagDto(Beer beer, List<HashTagTotalDto> hashTagTotalDtos) {
 		this.beerId = beer.getBeerId();
 		this.beerName = beer.getBeerName();
-		this.countryName = beer.getCountryName();
+		this.country = beer.getCountry();
 		this.beerCategory = beer.getBeerCategory();
 		this.alcoholProof = beer.getAlcoholProof();
 		this.beerImage = beer.getBeerImage();
--- backend/bear/src/main/java/com/zootopia/bear/Beer/service/BeerSearchService.java
+++ backend/bear/src/main/java/com/zootopia/bear/Beer/service/BeerSearchService.java
@@ -3,10 +3,12 @@ package com.zootopia.bear.Beer.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.zootopia.bear.Beer.domain.Beer;
 import com.zootopia.bear.Beer.dto.BeerHashTagDto;
 import com.zootopia.bear.Beer.repository.BeerRepository;
+import com.zootopia.bear.Country.domain.Country;
 import com.zootopia.bear.HashTag.dto.HashTagTotalDto;
 import com.zootopia.bear.HashTag.repository.HashTagRepository;
 
@@ -14,12 +16,13 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class BeerSearchService {
 	private final BeerRepository beerRepository;
 	private final HashTagRepository hashTagRepository;
 
 	public BeerHashTagDto beerSearch(int beerId) {
-		Beer beer = beerRepository.findById(beerId).orElseThrow(NullPointerException::new);
+		Beer beer = beerRepository.findById(beerId);
 		List<HashTagTotalDto> hashTagTotalDtos = hashTagRepository.searchHashTagToTal(beerId);
 		return new BeerHashTagDto(beer, hashTagTotalDtos);
 	}
