7ae43c745cb764d0ed8361f6ebaf02b5c8ff0c51
happ-in <gpqls304@naver.com>
2021-08-10 12:22:07 +0900

    Feat: Feat review search
    Signed-off-by: happ-in <gpqls304@naver.com>

---
+++ backend/bear/src/main/java/com/zootopia/bear/Review/controller/ReviewSearchController.java
@@ -0,0 +1,29 @@
+package com.zootopia.bear.Review.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zootopia.bear.Review.dto.ReviewDto;
+import com.zootopia.bear.Review.service.ReviewSearchService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/review")
+@RequiredArgsConstructor
+public class ReviewSearchController {
+
+	private final ReviewSearchService reviewSearchService;
+
+	@GetMapping
+	public ResponseEntity<?> MyReviewSearch(@RequestParam long userId) {
+		List<ReviewDto> reviewDtos = reviewSearchService.searchMyReview(userId);
+		return new ResponseEntity<>(reviewDtos, HttpStatus.OK);
+	}
+}
--- backend/bear/src/main/java/com/zootopia/bear/Review/repository/ReviewRepository.java
+++ backend/bear/src/main/java/com/zootopia/bear/Review/repository/ReviewRepository.java
@@ -1,5 +1,7 @@
 package com.zootopia.bear.Review.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.zootopia.bear.Review.domain.Review;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
+	List<Review> findAllByUserId(long userId);
 }
