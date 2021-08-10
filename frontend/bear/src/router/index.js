c20bc011c187bd5536f502941a6e85a1850cebab
yeji <yeahaluu@gmail.com>
2021-08-10 09:16:04 +0900

    Feat: Add ElementPlus

--- frontend/bear/src/router/index.js
+++ frontend/bear/src/router/index.js
@@ -2,6 +2,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 // import Home from "../views/Home.vue";
 import Recommendation from "../views/RecommendationPage.vue";
+import RankingMain from "../views/RankingMainPage.vue";
 
 const routes = [
   // {
@@ -9,20 +10,6 @@ const routes = [
   //   name: "Home",
   //   component: Home,
   // },
-  // {
-  //   path: "/about",
-  //   name: "About",
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () =>
-  //     import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  // },
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
   {
     path: "/ar",
     name: "Ar",
@@ -36,6 +23,11 @@ const routes = [
     path: "/recomendation",
     name: "Recomendation",
     component: Recommendation,
+  },
+  {
+    path: "/ranking",
+    name: "RankingMain",
+    component: RankingMain,
   },
 ];
 
