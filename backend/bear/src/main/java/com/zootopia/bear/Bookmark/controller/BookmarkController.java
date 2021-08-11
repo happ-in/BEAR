7b1142b03d8289610c2f475efaf52cbcbfcc813c
sangjun <june11215@naver.com>
2021-08-11 10:39:51 +0900

    Feat : Feat Bookmark

--- backend/bear/src/main/java/com/zootopia/bear/Bookmark/controller/BookmarkController.java
+++ backend/bear/src/main/java/com/zootopia/bear/Bookmark/controller/BookmarkController.java
@@ -1,11 +1,35 @@
 package com.zootopia.bear.Bookmark.controller;
 
 import com.zootopia.bear.Bookmark.service.BookmarkService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+
+@RestController
+@RequestMapping("/bookmark")
 public class BookmarkController {
     private final BookmarkService bookmarkService;
 
     public BookmarkController(BookmarkService bookmarkService) {
         this.bookmarkService = bookmarkService;
+    }
+
+    @GetMapping("/add")
+    public ResponseEntity<?> addBookmark(@RequestParam int beerId, HttpSession session) {
+        Long userId = (Long) session.getAttribute("userId");
+        bookmarkService.addBookmark(userId,beerId);
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @GetMapping("/delete")
+    public ResponseEntity<?> deleteBookmark(@RequestParam int beerId, HttpSession session) {
+        Long userId = (Long) session.getAttribute("userId");
+        bookmarkService.deleteBookmark(userId,beerId);
+        return new ResponseEntity<>(null, HttpStatus.OK);
     }
 }
--- backend/bear/src/main/java/com/zootopia/bear/Bookmark/service/BookmarkService.java
+++ backend/bear/src/main/java/com/zootopia/bear/Bookmark/service/BookmarkService.java
@@ -4,8 +4,9 @@ import com.zootopia.bear.Bookmark.domain.Bookmark;
 import com.zootopia.bear.Bookmark.domain.BookmarkId;
 import com.zootopia.bear.Bookmark.repository.BookmarkRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-
+@Service
 public class BookmarkService {
     private final BookmarkRepository bookmarkRepository;
 
