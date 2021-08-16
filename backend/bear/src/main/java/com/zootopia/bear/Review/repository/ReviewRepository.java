cd26d4f01b415caa3e632674dbfdad90d865f6f9
happ-in <gpqls304@naver.com>
2021-08-16 21:25:27 +0900

    Remove: Remove library not used
    Signed-off-by: happ-in <gpqls304@naver.com>

--- backend/bear/src/main/java/com/zootopia/bear/Review/repository/ReviewRepository.java
+++ backend/bear/src/main/java/com/zootopia/bear/Review/repository/ReviewRepository.java
@@ -25,7 +25,5 @@ public interface ReviewRepository extends JpaRepository<Review, Integer> {
 	@Query(value = "select * from review r join follow f on r.user_id = f.follow_user_id where f.user_id = :userId order by r.start_date", nativeQuery = true)
 	List<Review> searchFeed(@Param("userId") long userId);
 
-
-	public Optional<Review> findByBeerId(int beerId);
-
+	Optional<Review> findByBeerId(int beerId);
 }
