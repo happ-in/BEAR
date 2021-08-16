58b38850bb9c26b907a8da86c0a154a3e87e4c95
happ-in <gpqls304@naver.com>
2021-08-16 22:16:17 +0900

    Style: Edit SearchPage input style

--- frontend/bear/src/views/SearchPage.vue
+++ frontend/bear/src/views/SearchPage.vue
@@ -1,47 +1,29 @@
 <template>
   <div id="container">
-    <el-input
-      id="search_input"
-      class="inline-input"
-      v-model="keyword"
-      @input="submitAutoComplete"
-      :fetch-suggestions="querySearch"
-      placeholder="태그 또는 맥주를 입력하세요"
-      prefix-icon="el-icon-search"
-      @select="handleSelect"
-      style="width: 309px"
-    ></el-input>
+    <input id="search_input" class="search-input-style" v-model="keyword" placeholder="태그 또는 맥주를 입력하세요" />
 
-    <el-button-group style="margin-top: 60px">
+    <el-button-group style="margin-top: 2%">
       <el-button type="warning" plain style="width: 120px" @click="this.category = 'account'">계정</el-button>
       <el-button type="warning" plain style="width: 120px" @click="this.category = 'tag'"> 태그</el-button>
       <el-button type="warning" autofocus plain style="width: 120px" @click="this.category = 'beer'">맥주</el-button>
     </el-button-group>
-    <!-- 
-    <ul class="infinite-list" v-infinite-scroll="load" style="overflow: auto" v-if="category == 'beer'">
-      <li v-for="(beer, index) in beers" class="infinite-list-item" :key="index">
-        {{ beer.beerName }}
-      </li>
-    </ul>
-    
-    <ul class="infinite-list" v-infinite-scroll="load" style="overflow: auto" v-if="category == 'tag'">
-      <li v-for="(hashTag, index) in tags" class="infinite-list-item" :key="index">
-        {{ hashTag.hashTagName }}
-      </li>
-    </ul>
-     -->
+
+    <div v-if="category == 'account'" class="result-wrapper">
+      <div v-for="(user, index) in users" :key="index" class="mb-4">
+        <!-- <img :src="require('../assets/' + user.beerImage + '.png')" alt="" class="round-image beer-item" /> -->
+        <span>{{ user.customId }}</span>
+      </div>
+    </div>
 
     <div v-if="category == 'beer'" class="result-wrapper">
       <div v-for="(beer, index) in beers" :key="index" class="mb-4">
-        <!-- <el-avatar class="item" :size="40" :src="require('../assets/beers/' + beer.beerImage + '.png')"></el-avatar> -->
         <img :src="require('../assets/beers/' + beer.beerImage + '.png')" alt="" class="round-image beer-item" />
         <span>{{ beer.beerName }}</span>
       </div>
     </div>
 
     <div v-if="category == 'tag'" class="result-wrapper">
-      <div v-for="(hashTag, index) in tags" :key="index" class="mb-4" style="display: flex">
-        <!-- <span class="tag-image item">태그</span> -->
+      <div v-for="(hashTag, index) in tags" :key="index" class="mb-4 flex-box">
         <div class="tag-image">태그</div>
         <span class="tag-text"># {{ hashTag.hashTagName }}</span>
       </div>
@@ -61,10 +43,12 @@ export default defineComponent({
       count: 0,
       beers: [],
       tags: [],
+      users: [],
     };
   },
   methods: {
     async search() {
+      this.users = await this.$api("https://i5a403.p.ssafy.io/search/user?keyword=" + this.keyword, "get");
       this.beers = await this.$api("https://i5a403.p.ssafy.io/search/beer?keyword=" + this.keyword, "get");
       this.tags = await this.$api("https://i5a403.p.ssafy.io/search/hashtag?keyword=" + this.keyword, "get");
     },
@@ -96,12 +80,10 @@ export default defineComponent({
   background: white;
 }
 
-#search_input {
-  position: absolute;
-  width: 309px;
+.search-input-style {
+  width: -webkit-fill-available;
+  margin: 2%;
   height: 40px;
-  left: 0px;
-  top: 0px;
 }
 .round-image {
   width: 45px;
@@ -141,5 +123,8 @@ video {
 }
 .mb-4 {
   margin-bottom: 4%;
+}
+.flex-box {
+  display: flex;
 }
 </style>
