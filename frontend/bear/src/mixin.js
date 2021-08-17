9fffd4c1e53e5b6a85783fc2edac31c6b78ff771
happ-in <gpqls304@naver.com>
2021-08-17 18:47:06 +0900

    Refactor: Refactor axios baseURL

--- frontend/bear/src/mixin.js
+++ frontend/bear/src/mixin.js
@@ -1,15 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
+
+axios.defaults.baseURL = "https://i5a403.p.ssafy.io/";
 
 export default {
-    methods: {
-        async $api(url, method, data) {
-            return (await axios({
-                method: method,
-                url,
-                data
-            }).catch(e => {
-                console.log(e);
-            })).data;
-        }
-    }
-}
+  methods: {
+    async $api(url, method, data) {
+      return (
+        await axios({
+          method: method,
+          url,
+          data,
+        }).catch((e) => {
+          console.log(e);
+        })
+      ).data;
+    },
+  },
+};
