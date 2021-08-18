7ddd70653b166f2f6db5f1c8c964c06a77b69d97
happ-in <gpqls304@naver.com>
2021-08-19 01:10:53 +0900

    Fix: Fix bug

--- frontend/bear/src/views/LoginPage.vue
+++ frontend/bear/src/views/LoginPage.vue
@@ -1,11 +1,8 @@
 <template>
   <div id="container">
-    <img id="login-logo" src="../assets/landing/logo1.png"/>
+    <img id="login-logo" src="../assets/landing/logo1.png" />
     <a id="custom-login-btn" @click="kakaoLogin()">
-      <img id="login-btn"
-        src="//k.kakaocdn.net/14/dn/btqCn0WEmI3/nijroPfbpCa4at5EIsjyf0/o.jpg"
-        width="380"
-      />
+      <img id="login-btn" src="//k.kakaocdn.net/14/dn/btqCn0WEmI3/nijroPfbpCa4at5EIsjyf0/o.jpg" width="380" />
     </a>
   </div>
 </template>
@@ -24,27 +21,29 @@ export default {
     // this.kakaoLogout();
   },
   methods: {
-    kakaoLogin(){
-      window.location.replace("https://kauth.kakao.com/oauth/authorize?client_id=15e7887e87b724b15605d38adf95cf84&redirect_uri=https://i5a403.p.ssafy.io/login/kakao&response_type=code"); //test API
+    kakaoLogin() {
+      window.location.replace(
+        "https://kauth.kakao.com/oauth/authorize?client_id=15e7887e87b724b15605d38adf95cf84&redirect_uri=https://i5a403.p.ssafy.io/api/login/kakao&response_type=code"
+      ); //test API
     },
   },
 };
 </script>
 <style>
-  #login-logo{
-    position: absolute;
-    top: 30%;
-    left: 25%;
-    width:200px; 
-  }
-  #login-btn{
-    position: absolute;
-    width: 380px;
-    height: 60px;
-    top: 50%;
-    left: 5%; 
-  }
-  video{
-   display: none;
-  }
+#login-logo {
+  position: absolute;
+  top: 30%;
+  left: 25%;
+  width: 200px;
+}
+#login-btn {
+  position: absolute;
+  width: 380px;
+  height: 60px;
+  top: 50%;
+  left: 5%;
+}
+video {
+  display: none;
+}
 </style>
