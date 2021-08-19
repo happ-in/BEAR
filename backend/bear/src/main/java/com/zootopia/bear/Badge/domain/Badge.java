bb5d03f982a8647b2132224c8412a953c1ed23da
happ-in <gpqls304@naver.com>
2021-08-19 10:52:54 +0900

    Chore: Edit Entity column name

--- backend/bear/src/main/java/com/zootopia/bear/Badge/domain/Badge.java
+++ backend/bear/src/main/java/com/zootopia/bear/Badge/domain/Badge.java
@@ -3,6 +3,7 @@ package com.zootopia.bear.Badge.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -13,7 +14,13 @@ public class Badge {
     @Id
     private int badgeId;
     private String title;
+
+    @Column(name = "badge_explain")
     private String explain;
+
+    @Column(name = "badge_condition")
     private String condition;
+
+    @Column(name = "badge_Image")
     private String badgeImage;
 }
